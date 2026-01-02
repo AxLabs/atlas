d6740cc3ef8e136f8433ee2abde70a0ce02bf589
Daniel Mark <danielmark.uc@gmail.com>
2026-01-02 11:43:43 +0530

    fix: add suppressHydrationWarning to script tag for improved hydration handling

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -37,6 +37,7 @@ export default async function RootLayout({
       <head>
         <script
           nonce={nonce}
+          suppressHydrationWarning
           dangerouslySetInnerHTML={{
             __html: `
               (function() {
